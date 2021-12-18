ddb75d75ee385117288606c4f6e10f76fc2b8272
Anna Agafonova <aa.agafonova@gmail.com>
2021-12-18 22:02:18 +0300

    video + popup
    style and functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,51 @@
-/*
-function fullscreenBlock() {
-    let width = document.documentElement.clientWidth;
-    let height = document.documentElement.clientHeight;
-    let fullscreen = document.querySelector("#fullscreen");
+function videoPlay() {
+    let video = document.getElementById('player');
+    let playBtn = document.getElementById('play');
 
-    fullscreen.style.width = width + "px";
-    fullscreen.style.height = height + "px";
+    playBtn.onclick = function() {
+        if (video.paused) {
+            video.play();
+            playBtn.classList.add('hidden');
+        }
+    }
+    video.onclick = function() {
+        if (video.play) {
+            video.pause();
+            playBtn.classList.remove('hidden');
+        }
+    }
 }
 
-fullscreenBlock();
-window.onresize = fullscreenBlock;
+videoPlay();
+
+
+function popup() {
+    let body = document.querySelector('body');
+    let video = document.getElementById('player-popup');
+    let showBtn = document.getElementById('watch');
+    let popup = document.getElementById('popup');
+
+    showBtn.onclick = function() {
+        popup.classList.add('shown');
+        video.play();
+        body.classList.add('lock');
+    }
+    popup.onclick = function(event) {
+        if ((!video.contains(event.target))) {
+            popup.classList.remove('shown');
+            video.pause();
+            video.currentTime = 0;
+            body.classList.remove('lock');
+        }
+    }
+}
+
+popup();
+
+
+/*
+document.addEventListener('click', function(event) {
+  var e = document.getElementById('test');
+  if (!e.contains(event.target)) e.style.display='none';
+});
 */

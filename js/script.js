d56626367a9b6f51df8f8dceb855681546eeac1c
Anna Agafonova <aa.agafonova@gmail.com>
2021-12-19 18:25:13 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,49 +1,35 @@
-/*
-function topBg() {
-    let bg = document.getElementById('bg');
-    const column = document.getElementById('column');
-
-    const columnPosition = column.getBoundingClientRect();
-    bg.style.left = columnPosition.left + 'px';
-
-    console.log(columnPosition);
-
-}
-
-topBg();
-
-*/
-
-
-
 function videoPlay() {
-    let video = document.getElementById('player');
+    let videoPage = document.getElementById('player');
     let playBtn = document.getElementById('play');
-
-    playBtn.onclick = function() {
-        if (video.paused) {
-            video.play();
-            playBtn.classList.add('hidden');
-        }
-    }
-    video.onclick = function() {
-        if (video.play) {
-            video.pause();
-            playBtn.classList.remove('hidden');
-        }
-    }
-}
-
-function popup() {
+    let dark = document.getElementById('dark');
     let body = document.querySelector('body');
     let video = document.getElementById('player-popup');
     let showBtn = document.getElementById('watch');
     let popup = document.getElementById('popup');
 
+    playBtn.onclick = function() {
+        if (video.paused) {
+            videoPage.play();
+            playBtn.classList.add('hidden');
+            dark.classList.remove('shown');
+        }
+    }
+    videoPage.onclick = function() {
+        if (videoPage.play) {
+            videoPage.pause();
+            playBtn.classList.remove('hidden');
+            dark.classList.add('shown');
+        }
+    }
     showBtn.onclick = function() {
         popup.classList.add('shown');
         video.play();
         body.classList.add('lock');
+        if (videoPage.play) {
+            videoPage.pause();
+            dark.classList.add('shown');
+            playBtn.classList.remove('hidden');
+        }
     }
     popup.onclick = function(event) {
         if ((!video.contains(event.target))) {
@@ -56,5 +42,3 @@ function popup() {
 }
 
 videoPlay();
-popup();
-
